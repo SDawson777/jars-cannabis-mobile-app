c6818d2147446c25afb4fff69208ed7553698c9f
SDawson777 <dawsons1024@gmail.com>
2025-08-15 11:08:39 -0400

    chore(jest): use react-native matchers

--- jest.setup.js
+++
@@ -1,3 +0,0 @@
-jest.mock('@aws-amplify/analytics', () => ({ record: jest.fn() }));
-jest.mock('aws-amplify', () => ({ Analytics: { record: jest.fn() } }));
-globalThis.IS_REACT_ACT_ENVIRONMENT = true;
