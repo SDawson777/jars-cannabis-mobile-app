43f42ab17612e2474be0f1b69fc885629d706d8f
SDawson777 <dawsons1024@gmail.com>
2025-08-15 16:26:50 -0400

    test: isolate store context mocks

--- src/__tests__/deepLink.test.tsx
+++ src/__tests__/deepLink.test.tsx
@@ -1,3 +1,5 @@
+/* eslint-env jest, node */
+/* eslint-disable no-undef */
 import React from 'react';
 import { renderHook, waitFor, act } from '@testing-library/react-native';
 
@@ -9,14 +11,16 @@ jest.mock('react-native', () => ({
   },
 }));
 
+jest.mock('../context/StoreContext', () => {
+  const setPreferredStore = jest.fn();
+  return {
+    useStore: () => ({ setPreferredStore }),
+    StoreProvider: ({ children }: any) => <>{children}</>,
+    setPreferredStore,
+  };
+});
 import useDeepLinkHandler from '../hooks/useDeepLinkHandler';
 import { makeStore } from './testUtils';
-
-const setPreferredStore = jest.fn();
-jest.mock('../context/StoreContext', () => ({
-  useStore: () => ({ setPreferredStore }),
-  StoreProvider: ({ children }: any) => <>{children}</>,
-}));
 import { NavigationContainer } from '@react-navigation/native';
 jest.mock('expo-secure-store', () => ({
   getItemAsync: jest.fn(() => Promise.resolve(null)),
@@ -24,11 +28,16 @@ jest.mock('expo-secure-store', () => ({
   deleteItemAsync: jest.fn(() => Promise.resolve()),
 }));
 
+const { setPreferredStore } = require('../context/StoreContext') as any;
 const navigate = jest.fn();
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
   useNavigation: () => ({ navigate }),
 }));
+
+beforeEach(() => {
+  setPreferredStore.mockReset();
+});
 
 test('deep link loads Shop with correct store', async () => {
   const stores = [makeStore()];
